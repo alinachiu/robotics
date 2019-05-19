118ad82d09611775ec8a819a3e7176b378da05f7
xm22485 <48072293+xm22485@users.noreply.github.com>
2019-05-19 11:02:36 -0700

    Update SquirrelPython.py
    Type in the animal, and it will move the graphical face (in this folder) to respond.

--- SquirrelPython/SquirrelPython.py
+++ SquirrelPython/SquirrelPython.py
@@ -1,13 +1,33 @@
-file = open("pythonTest.txt", "w")
-ans = input("True or false?")
+file1 = open("everything.txt", "a")
 
-if ans == "true":
-    file.write("true")
-    file.close()
-elif ans == "false":
-    file.write("false")
-    file.close()
-else:
-    file.write("null")
-    file.close()
-
+while True:
+    ans = input("Pick an animal")
+    file1.close()
+    file1 = open("everything.txt", "w")
+    file1.close()
+    file1 = open("everything.txt", "a")
+    
+    if ans == "turtle":
+        file1.write("true\n")
+    else:
+        file1.write("false\n")
+    if ans == "fish":
+        file1.write("true\n")
+    else:
+        file1.write("false\n")
+    if ans == "dog":
+        file1.write("true\n")
+    else:
+        file1.write("false\n")
+    if ans == "horse":
+        file1.write("true\n")
+    else:
+        file1.write("false\n")
+    if ans == "wolf":
+        file1.write("true")
+    else:
+        file1.write("false")
+    if ans == "break":
+        file1.close()
+        break
+    file1.close()
